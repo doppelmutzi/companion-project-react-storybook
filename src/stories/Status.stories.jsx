d15c41c4e739ba1e33dd16e22f6b2b60b1f2e33c
Sebastian Weber <sebatian.weber80@gmail.com>
2021-11-16 21:44:22 +0100

    2 stories

--- src/stories/Status.stories.jsx
+++ src/stories/Status.stories.jsx
@@ -3,6 +3,12 @@ import AppContext from "../AppContext";
 import React from "react";
 import Status from "../Status";
 
+/**
+ As shown above, you can configure individual controls with the “control" annotation in the argTypes field of either a component or story. 
+ */
+
+// https://storybook.js.org/docs/react/essentials/controls
+
 // https://storybook.js.org/docs/react/api/csf
 // https://storybook.js.org/docs/react/writing-stories/decorators
 
@@ -30,40 +36,83 @@ export default {
   //     { id: Date.now(), label: "a todo", checked: false },
   //   ]),
   // ],
-
-  // Todo wie bei emoji
-
-  // argTypes: {
-  //   todos: {
-  //     options: [
-  //       [
-  //         { id: Date.now(), label: "a todo", checked: false },
-  //         { id: Date.now(), label: "a todo", checked: false },
-  //       ],
-  //       [
-  //         { id: Date.now(), label: "a todo", checked: false },
-  //         { id: Date.now(), label: "a todo", checked: true },
-  //       ],
-  //     ],
-  //     control: "radio",
-  //   },
-  // },
-  args: {
-    checked: false,
+  args: {},
+  argTypes: {
+    // checked: {
+    //   name: "name",
+    //   description: "description",
+    //   defaultValue: false,
+    //   control: "boolean",
+    // },
   },
 };
 
-const StatusComponent = ({ checked }) => (
-  <AppContext.Provider
-    value={{
-      todos: [
-        { id: Date.now(), label: "a todo", checked },
-        { id: Date.now(), label: "a todo", checked },
-      ],
-    }}
-  >
-    <Status />
-  </AppContext.Provider>
-);
+const StatusComponent = ({ todosContextValue }) => {
+  return (
+    <AppContext.Provider
+      value={{
+        todos: todosContextValue,
+      }}
+    >
+      <Status />
+    </AppContext.Provider>
+  );
+};
 
-export const Basic = (args) => <StatusComponent {...args} />;
+const Template = (args) => <StatusComponent {...args} />;
+
+export const StatusBasic = Template.bind({});
+StatusBasic.argTypes = {
+  todosContextValue: {
+    name: "list of todos",
+    description:
+      "Variants of todo lists with different check states. Status component shows number of unchecked todos.",
+    defaultValue: "openTodos",
+    table: {
+      // type: { summary: "list of todos" },
+      // defaultValue: { summary: "blub" },
+    },
+    options: [
+      "openTodos",
+      "partlyCompletedTodos",
+      "allCompletedTodos",
+      "noTodos",
+    ],
+    mapping: {
+      openTodos: [
+        { id: Date.now(), label: "a todo", checked: false },
+        { id: Date.now(), label: "a todo", checked: false },
+      ],
+      partlyCompletedTodos: [
+        { id: Date.now(), label: "a todo", checked: false },
+        { id: Date.now(), label: "a todo", checked: true },
+      ],
+      allCompletedTodos: [
+        { id: Date.now(), label: "a todo", checked: true },
+        { id: Date.now(), label: "a todo", checked: true },
+      ],
+      noTodos: [],
+    },
+    control: {
+      type: "select", // Type 'select' is automatically inferred when 'options' is defined
+      labels: {
+        // 'labels' maps option values to string labels
+        noTodos: "No todos in list",
+        openTodos: "2 of 2 open todos",
+        partlyCompletedTodos: "1 of 2 open todos",
+        allCompletedTodos: "2 of 2 completed todos",
+      },
+    },
+  },
+};
+
+export const StatusInteractive = Template.bind({});
+// TODO wie Name definieren?
+StatusInteractive.argTypes = {
+  todosContextValue: {
+    name: "list of todos",
+    description: "Status component shows number of unchecked todos.",
+    defaultValue: [{ id: Date.now(), label: "a todo", checked: false }],
+    control: { type: "object" },
+  },
+};
