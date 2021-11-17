d219c0ebb8724743e3d057a01cf8b098ea3358b0
Sebastian Weber <sebatian.weber80@gmail.com>
2021-11-17 21:01:31 +0100

    show controls also in docs view

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,7 @@
 module.exports = {
   core: {
-    builder: 'webpack5',
+    builder: "webpack5",
   },
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    'storybook-addon-jsx',
-    '@storybook/addon-controls',
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["storybook-addon-jsx", "@storybook/addon-actions", "@storybook/addon-essentials", "@storybook/addon-controls"],
+};
